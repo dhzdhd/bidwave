ed195a605f950c87208cb2da2fdd08c0432136a3
dhzdhd <ragas5432100@gmail.com>
2023-09-16 19:56:10 +0530

    Add sass and server file for products

---
+++ frontend/src/routes/home/+page.server.ts
@@ -0,0 +1,60 @@
+import type { PageServerLoad } from './$types';
+
+const apiUrl = 'http://localhost:1337/api';
+
+interface Product {
+	id: number;
+	name: string;
+	auctionEnd: string; // Change to date
+	auctionStart: string;
+	price: number;
+	bidPrice: number;
+	amount: number;
+	description: string;
+	available: boolean;
+	image: Image;
+}
+
+interface Image {
+	alt: string;
+	width: number;
+	height: number;
+	mime: string;
+	url: string;
+}
+
+export const load: PageServerLoad = async ({ cookies }) => {
+	const response = await fetch(`${apiUrl}/products?populate=*`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${cookies.get('sessionJwt')}`
+		}
+	});
+
+	const json = await response.json();
+
+	return {
+		products: json.data.map((e: any) => {
+			const rawImage = e['attributes']['image']['data'][0]['attributes'];
+
+			return {
+				id: e['id'],
+				name: e['attributes']['name'],
+				auctionEnd: e['attributes']['auction_end'],
+				auctionStart: e['attributes']['auction_start'],
+				price: e['attributes']['price'],
+				bidPrice: e['attributes']['bid_price'],
+				amount: e['attributes']['number'],
+				description: e['attributes']['description'],
+				available: e['attributes']['available'],
+				image: {
+					alt: rawImage['name'],
+					width: rawImage['width'],
+					height: rawImage['height'],
+					mime: rawImage['mime'],
+					url: rawImage['url']
+				} satisfies Image
+			} satisfies Product;
+		})
+	} satisfies { products: Product[] };
+};
